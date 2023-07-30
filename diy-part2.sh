dfb160102f5aecdb8244509693a2d894f103bbb9
Simon Wei(位西朋) <perfectnewer@users.noreply.github.com>
2023-07-30 13:58:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='asus-wrt'/g" ./package/base-files/files/bin/config_generate
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
